d002f6d52920bd338d426357567e963f1d70e302
Emerson Carneiro da Silva <mersiocarneiro87@gmail.com>
2025-11-22 23:43:16 -0300

    feat: configure Next.js image remote patterns for Supabase public storage

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     remotePatterns: [
-      // {
-      //   protocol: "https",
-      //   hostname: "via.placeholder.com",
-      // },
-      // {
-      //   protocol: "https",
-      //   hostname: "images.pexels.com",
-      // },
       {
-        // A nível de teste
         protocol: "https",
-        hostname: "**",
+        hostname: "*.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/**",
       },
     ],
   },
